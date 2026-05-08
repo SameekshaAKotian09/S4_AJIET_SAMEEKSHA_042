1b887b8ba30afbd8bbc0387394d768b20a1f93f7
SameekshaAKotian09 <sameekshakotian0@gmail.com>
2026-05-08 14:55:43 +0530

    Create arrayexample.java

---
+++ arrayexample.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[]args){
+    int[] arr={7,8,13,15};
+     System.out.println("Elements are");
+    for(int i=0;i<arr.length;i++){
+    System.out.println(arr[i]);
+}
+}
+}
