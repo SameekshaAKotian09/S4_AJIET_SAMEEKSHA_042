38d6083173186a9de000fdf9d0b021dd69550139
SameekshaAKotian09 <sameekshakotian0@gmail.com>
2026-05-08 15:23:07 +0530

    Create smallestelement.java

---
+++ smallestelement.java
@@ -0,0 +1,12 @@
+public class Main{
+ public static void main(String[] args) {
+        int[] arr = {17,8,25,2};
+        int smallest = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] <smallest) {
+               smallest = arr[i];
+            }
+        }
+        System.out.println("Smallest element = " + smallest);
+    }
+}
